cfde8139322c8f6c25953ef41480c39840e0196d
TheVictorHugo <victorq32@gmail.com>
2022-08-26 10:57:52 -0300

    1 file modified

--- index.js
+++ index.js
@@ -78,3 +78,4 @@ function gerarNumerosentre(min,max){
 gerarNumerosentre(1, 60).then(num => num * 10).then(numX10 => `O numero gerado foi ${numX10}`).then(console.log)
 
 ////////////////////////////
+
